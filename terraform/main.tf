e1c89d037025f56c1f74be2190aca9a3a744c1ce
Shankar M <msshankar240698@gmail.com>
2025-06-10 12:32:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,16 @@ provider "aws" {
 resource "aws_instance" "rails_app" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  availability_zone = var.availability_zone
+  availability_zone      = var.availability_zone
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
+
+  root_block_device {
+    volume_size = var.vol_size         # Size in GB
+    volume_type = var.vol_type  
+    delete_on_termination = true
+  }
 
   tags = {
     Name = "Report Builder"
